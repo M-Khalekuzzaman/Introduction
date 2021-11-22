021e712292eeca0e9b53fbf7434d41d83283bb7b
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-22 20:55:49 +0600

    Add files via upload

---
+++ Introduction.cpp
@@ -0,0 +1,22 @@
+/*
+OOP = Object Oriented Programming
+C = OOP = C++,Python
+1)Class
+2)Object
+3) Inheritance
+4)Abstraction
+5)Polymorphism
+6)Encapsulation
+7)Contractors
+8)Destructors
+*/
+//Class = A class is a template from which individual object can be create
+//Object = Any class type variable is called an object
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    cout<<"Hello world !!!";
+    return 0 ;
+}
